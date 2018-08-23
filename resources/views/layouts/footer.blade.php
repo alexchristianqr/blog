1dee48b6886d3cd77637dd12c305c480d18c0513
Alex Christian <aquispe.developer@gmail.com>
2018-08-23 02:25:38 -0500

    Corregido bug en footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,10 @@
-<footer class="py-sm-5 py-lg-5 bg-light border-left-0 border-right-0 border-bottom-0" style="border: 1px solid #6c757d;">
-    <footer-layout></footer-layout>
-</footer>
+
+@if(request()->routeIs('route.blog.post'))
+    <footer class="py-sm-5 py-lg-5 bg-light border-left-0 border-right-0 border-bottom-0" style="border: 1px solid #6c757d;">
+        <footer-layout></footer-layout>
+    </footer>
+    @else
+    <footer class="py-5 bg-light border-left-0 border-right-0 border-bottom-0" style="border: 1px solid #6c757d;">
+        <footer-layout></footer-layout>
+    </footer>
+@endif
