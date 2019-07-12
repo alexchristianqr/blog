6d3537591618e344877e341c8b97c60462c0beb6
Alex Christian <aquispe.developer@gmail.com>
2019-07-12 18:14:14 -0500

    Modificado controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Exception;
 
@@ -71,7 +72,7 @@ class RegisterController extends Controller
                     'name' => $request->full_name,
                     'email' => $request->email,
                     'pwd_decrypted' => $request->password,
-                    'password' => bcrypt($request->password),
+                    'password' => Hash::make($request->password),
                 ]);
                 return redirect()->route('get.login')->withInput(['email' => $request->email])->with(['message_success'=>'User created successfully']);
             }else{
