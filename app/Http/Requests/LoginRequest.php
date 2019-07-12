acc17c26e1821ee86544222ff02987e018da0aaf
Alex Christian <aquispe.developer@gmail.com>
2019-07-12 18:26:49 -0500

    Modificado login request

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -25,7 +25,7 @@ class LoginRequest extends FormRequest
     {
         return [
             'email' => 'required|email',
-            'password' => 'required'
+            'password' => 'required|string|min:8|max:16'
         ];
     }
 }
