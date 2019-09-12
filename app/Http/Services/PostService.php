0c8325880a7d44f00e4653e142f0efafb676122e
Alex Christian <aquispe.developer@gmail.com>
2019-09-12 01:34:50 -0500

    Agregado filtro por rango de fecha

--- app/Http/Services/PostService.php
+++ app/Http/Services/PostService.php
@@ -13,213 +13,206 @@ use Illuminate\Support\Facades\DB;
 
 class PostService
 {
-    use Utility;
+	use Utility;
 
-    private $paginateGlobal = 4;
+	private $paginateGlobal = 4;
 
-    private function dataModel($request)
-    {
-        //Data Model
-        if($request->has('fields')){
-            $dataModel = Post::select($request->fields);
-        }else{
-            $dataModel = Post::select('post.*', 'path.name AS path_name', 'users.name AS user_name');
-        }
+	private function dataModel($request)
+	{
+		//Data Model
+		if($request->has('fields')){
+			$dataModel = Post::select($request->fields);
+		}else{
+			$dataModel = Post::select('post.*', 'path.name AS path_name', 'users.name AS user_name');
+		}
+		$dataModel = $dataModel->join('path', 'path.id', 'post.path_id')->join('users', 'users.id', 'post.user_id');
+		//Rango de Fecha
+		if($request->request->has('date_range')){
+			$dateExplode = explode('/', $request->request->get('date_range'));
+			if($request->ajax()){//Vue
+				$dataModel = $dataModel->whereBetween(DB::raw('DATE(post.updated_at)'), [$dateExplode[0], $dateExplode[1]]);
+			}else{//Laravel
+				$dataModel = $dataModel->whereBetween(DB::raw('DATE(post.published)'), [$dateExplode[0], $dateExplode[1]]);
+			}
+		}
+		//Year
+		if($request->request->has('year')) $dataModel = $dataModel->whereYear('post.published', $request->year);
+		//Month
+		if($request->request->has('month')) $dataModel = $dataModel->whereMonth('post.published', $request->month);
+		//Search
+		if($request->has('param_search')){
+			if(strpos($request->param_search, ' ') == true){
+				$simbol = ' ';
+			}else if(strpos($request->param_search, '+') == true){
+				$simbol = '+';
+			}else{
+				$simbol = ' ';
+			}
+			$research_text = explode($simbol, $request->param_search);
+			if(count($research_text) > 1){
+				$dataModel = $dataModel->where(function($query) use ($research_text){
+					//Filtrar por el campo name del post
+					$query = $query->where('post.name', 'like', '%' . $research_text[0] . '%');
+					foreach($research_text as $k => $v){
+						if(isset($research_text[$k + 1])){
+							$query = $query->orWhere('post.name', 'like', '%' . $research_text[$k + 1] . '%');
+						}
+					}
+					//Filtrar por el campo descripcion del post
+					$query = $query->orWhere('post.description', 'like', '%' . $research_text[0] . '%');
+					foreach($research_text as $k => $v){
+						if(isset($research_text[$k + 1])){
+							$query = $query->orWhere('post.description', 'like', '%' . $research_text[$k + 1] . '%');
+						}
+					}
+					//Filtrar por el campo name del usuario
+					$query = $query->orWhere('users.name', 'like', '%' . $research_text[0] . '%');
+					foreach($research_text as $k => $v){
+						if(isset($research_text[$k + 1])){
+							$query = $query->orWhere('users.name', 'like', '%' . $research_text[$k + 1] . '%');
+						}
+					}
+				});
+			}else{
+				$dataModel = $dataModel->where(function($query) use ($request){
+					$query->where('post.name', 'like', '%' . $request->param_search . '%');
+					$query->orWhere('users.name', 'like', '%' . $request->param_search . '%');
+				});
+			}
+		}
 
-        $dataModel = $dataModel->join('path', 'path.id', 'post.path_id')
-            ->join('users', 'users.id', 'post.user_id');
+		//User_id
+		if($request->has('user_id')) $dataModel = $dataModel->where('users.id', $request->user_id);
 
-        //Range
-        if($request->has('dateFilterStart') && $request->has('dateFilterEnd')){
-            if($request->ajax()){//Vue
-                $dataModel = $dataModel->whereBetween(DB::raw('DATE(post.updated_at)'), [$request->dateFilterStart, $request->dateFilterEnd]);
-            }else{//Laravel
-                $dataModel = $dataModel->whereBetween(DB::raw('DATE(post.published)'), [$request->dateFilterStart, $request->dateFilterEnd]);
-            }
-        }
+		//Post_id
+		if($request->has('post_id')) $dataModel = $dataModel->where('post.kind', $request->post_id);
 
-        //Year
-        if($request->has('year')) $dataModel = $dataModel->whereYear('post.published', $request->year);
+		//Status
+		if($request->has('status')) $dataModel = $dataModel->where('post.status', $request->status);
 
-        //Month
-        if($request->has('month')) $dataModel = $dataModel->whereMonth('post.published', $request->month);
+		//OrderField
+		if($request->has('orderBy')) $dataModel = $dataModel->orderBy(($request->has('orderField')) ? $request->orderField : 'post.updated_at', 'DESC');
 
-        //Search
-        if($request->has('param_search')){
-            if(strpos($request->param_search, ' ') == true){
-                $simbol = ' ';
-            }else if(strpos($request->param_search, '+') == true){
-                $simbol = '+';
-            }else{
-                $simbol = ' ';
-            }
-            $research_text = explode($simbol, $request->param_search);
-            if(count($research_text) > 1){
-                $dataModel = $dataModel->where(function($query) use ($research_text){
-                    //Filtrar por el campo name del post
-                    $query = $query->where('post.name', 'like', '%' . $research_text[0] . '%');
-                    foreach($research_text as $k => $v){
-                        if(isset($research_text[$k + 1])){
-                            $query = $query->orWhere('post.name', 'like', '%' . $research_text[$k + 1] . '%');
-                        }
-                    }
-                    //Filtrar por el campo descripcion del post
-                    $query = $query->orWhere('post.description', 'like', '%' . $research_text[0] . '%');
-                    foreach($research_text as $k => $v){
-                        if(isset($research_text[$k + 1])){
-                            $query = $query->orWhere('post.description', 'like', '%' . $research_text[$k + 1] . '%');
-                        }
-                    }
-                    //Filtrar por el campo name del usuario
-                    $query = $query->orWhere('users.name', 'like', '%' . $research_text[0] . '%');
-                    foreach($research_text as $k => $v){
-                        if(isset($research_text[$k + 1])){
-                            $query = $query->orWhere('users.name', 'like', '%' . $research_text[$k + 1] . '%');
-                        }
-                    }
-                });
-            }else{
-                $dataModel = $dataModel->where(function($query) use ($request){
-                    $query->where('post.name', 'like', '%' . $request->param_search . '%');
-                    $query->orWhere('users.name', 'like', '%' . $request->param_search . '%');
-                });
-            }
-        }
+		//Debug Customize
+		//$this->myDebug($dataModel,true);
 
-        //User_id
-        if($request->has('user_id')) $dataModel = $dataModel->where('users.id', $request->user_id);
+		//Paginate
+		if($request->has('paginate')){
+			if($request->ajax()){//Javascript
+				return $dataModel->paginate($request->paginate);
+			}else{//Php
+				if($request->has('simplePaginate')){//Si es un paginado simple
+					return $dataModel->simplePaginate($request->paginate);
+				}else{//Si es un paginado enumerado
+					return $dataModel->paginate($request->paginate);
+				}
+			}
+		}
+		//First
+		if($request->has('first')) return $dataModel->first();
+		//Get
+		else return $dataModel->get();
+	}
 
-        //Post_id
-        if($request->has('post_id')) $dataModel = $dataModel->where('post.kind', $request->post_id);
+	function getPosts($request)
+	{
+		$request->request->add([
+			'paginate' => $this->paginateGlobal,
+			'status' => 'A',
+			'simplePaginate' => true,
+			'orderBy' => true,
+		]);
+		return $this->dataModel($request);
+	}
 
-        //Status
-        if($request->has('status')) $dataModel = $dataModel->where('post.status', $request->status);
+	function getAll($request)
+	{
+		$request->request->add(['orderBy' => true]);
+		return $this->dataModel($request);
+	}
 
-        //OrderField
-        if($request->has('orderBy')) $dataModel = $dataModel->orderBy(($request->has('orderField')) ? $request->orderField : 'post.updated_at', 'DESC');
+	function getPostById($year, $month, $post_id, $request)
+	{
+		$request->request->add([
+			'first' => true,
+			'year' => $year,
+			'month' => $month,
+			'post_id' => $post_id,
+			'status' => 'A',
+		]);
+		return $this->dataModel($request);
+	}
 
-        //Debug Customize
-        //$this->myDebug($dataModel,true);
+	function create($request)
+	{
+		return (new Post())->fill($request->all())->save();
+	}
 
-        //Paginate
-        if($request->has('paginate')){
-            if($request->ajax()){//Javascript
-                return $dataModel->paginate($request->paginate);
-            }else{//Php
-                if($request->has('simplePaginate')){//Si es un paginado simple
-                    return $dataModel->simplePaginate($request->paginate);
-                }else{//Si es un paginado enumerado
-                    return $dataModel->paginate($request->paginate);
-                }
-            }
-        }
+	function update($post_id, $request)
+	{
+		return Post::find($post_id)->fill($request->all())->save();
+	}
 
-        //First
-        if($request->has('first')) return $dataModel->first();
+	function getHistory()
+	{
+		$newDataPostHistory = [];
+		$arrayMonths = [];
+		$arrayUrls = [];
+		$months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+		$years = $this->generateYearsRange('2000-01-01', Carbon::now());
+		$dataPostHistory = Post::select(DB::raw('YEAR(post.published) AS year, MONTH(post.published) AS month'), 'post.kind', 'post.name')
+			->where('post.status', 'A')
+			->orderBy('post.published', 'DESC')
+			->get();
+		$currentYear = 0;
+		$currentMonth = 0;
+		//Ciclo Year
+		foreach($dataPostHistory as $item){
+			if($item->year != $currentYear){
+				$currentYear = 0;//Reinicializar la variable del año
+				$arrayMonths = [];//Reinicializar el arreglo de meses que se encuentra en el año
+			}
+			for($k = 0; $k < count($years); $k++){
+				if($item->year == $years[$k]){
+					$currentYear = $item->year;
+					if($item->month != $currentMonth){
+						$currentMonth = 0;//Reinicializar la variable del mes
+					}
+					for($i = 0; $i < count($months); $i++){
+						if($item->month == $months[$i]){
+							$arrayUrlsTemp = $arrayUrls;//Cargar arreglo de links-temporales del arreglo de links
+							$arrayUrls = [];//Reinicializar el arreglo de links qu se encuentra en el mes
+							if($currentMonth == $months[$i]){
+								array_push($arrayUrlsTemp, (object)['kind' => $item->kind, 'name' => $item->name]);
+								$arrayUrls = $arrayUrlsTemp;//Cargar arreglo de links del arreglo de links-temporales
+							}else{
+								$currentMonth = $item->month;
+								array_push($arrayUrls, (object)['kind' => $item->kind, 'name' => $item->name]);
+							}
+							$arrayMonths[$months[$i]] = $arrayUrls;//Almacenar links en el mes del ciclo
+						}else{
+							continue;
+						}
+					}
+				}else{
+					continue;
+				}
+			}
+			$newDataPostHistory[$item->year] = $arrayMonths;
+			ksort($newDataPostHistory[$item->year]);
+		}
+		return $newDataPostHistory;
+	}
 
-        //Get
-        else return $dataModel->get();
-    }
+	function getSearch($request)
+	{
+		$request->request->add(['paginate' => $this->paginateGlobal, 'status' => 'A']);
+		return $this->dataModel($request);
+	}
 
-    function getPosts($request)
-    {
-        $request->request->add([
-            'paginate' => $this->paginateGlobal,
-            'status' => 'A',
-            'simplePaginate' => true,
-            'orderBy' => true,
-        ]);
-        return $this->dataModel($request);
-    }
-
-    function getAll($request)
-    {
-        $request->request->add(['orderBy' => true]);
-        return $this->dataModel($request);
-    }
-
-    function getPostById($year, $month, $post_id, $request)
-    {
-        $request->request->add([
-            'first' => true,
-            'year' => $year,
-            'month' => $month,
-            'post_id' => $post_id,
-            'status' => 'A',
-        ]);
-        return $this->dataModel($request);
-    }
-
-    function create($request)
-    {
-        return (new Post())->fill($request->all())->save();
-    }
-
-    function update($post_id, $request)
-    {
-        return Post::find($post_id)->fill($request->all())->save();
-    }
-
-    function getHistory()
-    {
-        $newDataPostHistory = [];
-        $arrayMonths = [];
-        $arrayUrls = [];
-        $months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-        $years = $this->generateYearsRange('2000-01-01', Carbon::now());
-        $dataPostHistory = Post::select(DB::raw('YEAR(post.published) AS year, MONTH(post.published) AS month'), 'post.kind', 'post.name')
-            ->where('post.status', 'A')
-            ->orderBy('post.published', 'DESC')
-            ->get();
-        $currentYear = 0;
-        $currentMonth = 0;
-        //Ciclo Year
-        foreach($dataPostHistory as $item){
-            if($item->year != $currentYear){
-                $currentYear = 0;//Reinicializar la variable del año
-                $arrayMonths = [];//Reinicializar el arreglo de meses que se encuentra en el año
-            }
-            for($k = 0; $k < count($years); $k++){
-                if($item->year == $years[$k]){
-                    $currentYear = $item->year;
-                    if($item->month != $currentMonth){
-                        $currentMonth = 0;//Reinicializar la variable del mes
-                    }
-                    for($i = 0; $i < count($months); $i++){
-                        if($item->month == $months[$i]){
-                            $arrayUrlsTemp = $arrayUrls;//Cargar arreglo de links-temporales del arreglo de links
-                            $arrayUrls = [];//Reinicializar el arreglo de links qu se encuentra en el mes
-                            if($currentMonth == $months[$i]){
-                                array_push($arrayUrlsTemp, (object)['kind' => $item->kind, 'name' => $item->name]);
-                                $arrayUrls = $arrayUrlsTemp;//Cargar arreglo de links del arreglo de links-temporales
-                            }else{
-                                $currentMonth = $item->month;
-                                array_push($arrayUrls, (object)['kind' => $item->kind, 'name' => $item->name]);
-                            }
-                            $arrayMonths[$months[$i]] = $arrayUrls;//Almacenar links en el mes del ciclo
-                        }else{
-                            continue;
-                        }
-                    }
-                }else{
-                    continue;
-                }
-            }
-            $newDataPostHistory[$item->year] = $arrayMonths;
-            ksort($newDataPostHistory[$item->year]);
-        }
-        return $newDataPostHistory;
-    }
-
-    function getSearch($request)
-    {
-        $request->request->add(['paginate' => $this->paginateGlobal, 'status' => 'A']);
-        return $this->dataModel($request);
-    }
-
-    function getLatestPosts($request)
-    {
-        $request->request->add(['fields' => ['image', 'path.name AS path_name', 'post.name'], 'status' => 'A']);
-        return $this->dataModel($request);
-    }
+	function getLatestPosts($request)
+	{
+		$request->request->add(['fields' => ['image', 'path.name AS path_name', 'post.name'], 'status' => 'A']);
+		return $this->dataModel($request);
+	}
 }
--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -8,45 +8,47 @@ namespace App\Http\Services;
 
 use App\User;
 use Exception;
+use Illuminate\Support\Facades\DB;
 
 class UserService
 {
-    private function dataModel($request)
-    {
-        //Data Model
-        $dataModel = User::select(['users.*']);
+	private function dataModel($request)
+	{
+		//Data Model
+		$dataModel = User::select(['users.*']);
+		//Rango de Fecha
+		if($request->request->has('date_range')){
+			$dateExplode = explode('/', $request->request->get('date_range'));
+			$dataModel = $dataModel->whereBetween(DB::raw('DATE(users.updated_at)'), [$dateExplode[0], $dateExplode[1]]);
+		}
+		//User_id
+		if($request->has('user_id')) $dataModel = $dataModel->where('users.id', $request->user_id);
+		//Paginate
+		if($request->has('paginate')) return $dataModel->paginate($request->paginate);
+		//First
+		if($request->has('first')) return $dataModel->first();
+		//Get
+		else return $dataModel->get();
+	}
 
-        //User_id
-        if($request->has('user_id')) $dataModel = $dataModel->where('users.id', $request->user_id);
+	function getUsers($request)
+	{
+		return $this->dataModel($request);
+	}
 
-        //Paginate
-        if($request->has('paginate')) return $dataModel->paginate($request->paginate);
+	function getUserById($request)
+	{
+		return $this->dataModel($request);
+	}
 
-        //First
-        if($request->has('first')) return $dataModel->first();
+	function create($request)
+	{
+		return (new User())->fill($request->all())->save();
+	}
 
-        //Get
-        else return $dataModel->get();
-    }
-
-    function getUsers($request)
-    {
-        return $this->dataModel($request);
-    }
-
-    function getUserById($request)
-    {
-        return $this->dataModel($request);
-    }
-
-    function create($request)
-    {
-        return (new User())->fill($request->all())->save();
-    }
-
-    function update($request)
-    {
-        return User::find($request->user_id)->fill($request->all())->save();
-    }
+	function update($request)
+	{
+		return User::find($request->user_id)->fill($request->all())->save();
+	}
 
 }
