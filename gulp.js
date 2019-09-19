bc3721f404c725236a4690c35a681149d4263c76
Alex Christian <aquispe.developer@gmail.com>
2019-09-19 07:10:47 -0500

    Modificado app.js

--- gulp.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp')
-const gulp_bootstrap_email = require('gulp-bootstrap-email')
-
-gulp.task('email', () => {
-  return gulp.src('./resources/views/templates/subscription/file.html').
-    pipe(gulp_bootstrap_email()).pipe(gulp.dest('./resources/views/templates/subscription/template-subscription-html.blade.php'))
-})
--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,12 +1,5 @@
-// const BootstrapEmail = require('bootstrap-email');
-// import BootstrapEmail from 'bootstrap-email'
-// const template = new BootstrapEmail({
-//   template: './resources/views/templates/subscription/file.html'
-// });
-// template.compileAndSave('./public/dist/plantilla.html');
-
 import Vue from 'vue'
-import $   from 'jquery'
+import $ from 'jquery'
 import 'social-share-kit'
 import 'bootstrap'
 
@@ -18,36 +11,36 @@ Vue.config.debug = false
 Vue.config.silent = true
 
 new Vue({
-    created(){
-        let doc = document.location.pathname.replace('/','').split('/')
-        if(doc.length == 5 && doc[0] == 'blog' && doc[1] == 'post'){
-            this.initFacebook()
-            this.initShareKit()
-        }
+  created(){
+    let doc = document.location.pathname.replace('/', '').split('/')
+    if(doc.length == 5 && doc[0] == 'blog' && doc[1] == 'post'){
+      this.initFacebook()
+      this.initShareKit()
+    }
+  },
+  methods: {
+    initFacebook(){
+      window.fbAsyncInit = () => {
+        FB.init({
+          appId: '481663735685291',
+          xfbml: true,
+          version: 'v3.1',
+        })
+        FB.AppEvents.logPageView()
+      };
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0]
+        if(d.getElementById(id)){return}
+        js = d.createElement(s)
+        js.id = id
+        js.src = 'https://connect.facebook.net/en_US/sdk.js'
+        fjs.parentNode.insertBefore(js, fjs)
+      }(document, 'script', 'facebook-jssdk'))
     },
-    methods: {
-        initFacebook(){
-            window.fbAsyncInit = () => {
-                FB.init({
-                    appId: '481663735685291',
-                    xfbml: true,
-                    version: 'v3.1',
-                })
-                FB.AppEvents.logPageView()
-            };
-            (function(d, s, id){
-                    var js, fjs = d.getElementsByTagName(s)[0]
-                    if(d.getElementById(id)){return}
-                    js = d.createElement(s)
-                    js.id = id
-                    js.src = 'https://connect.facebook.net/en_US/sdk.js'
-                    fjs.parentNode.insertBefore(js, fjs)
-                }(document, 'script', 'facebook-jssdk'))
-        },
-        initShareKit(){
-            $(document).ready(() =>{
-                SocialShareKit.init()
-            })
-        },
+    initShareKit(){
+      $(document).ready(() => {
+        SocialShareKit.init()
+      })
     },
+  },
 }).$mount('#app')
