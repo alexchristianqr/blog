ba2209e2f7a279459e7c65f07c7547bec3f606d5
Alex Christian <aquispe.developer@gmail.com>
2019-09-24 17:33:48 -0500

    Creado alert anv warning

--- resources/views/layouts/nav/nav-beta.blade.php
+++ resources/views/layouts/nav/nav-beta.blade.php
@@ -2,10 +2,8 @@
     <div class="container">
         <div class="alert alert-primary fade show border-0 pl-0 pr-0 mb-0" role="alert" style="border-radius: 0">
             <div class="row">
-                <div class="col-11 my-auto">
+                <div class="col-12 my-auto">
                     <span class="my-auto">This website is deployed in <span class="mark border-1 border-primary rounded bg-transparent text-primary"><span class="font-weight-light mr-1 ml-1">Mode Beta</span></span></span>
-                </div>
-                <div class="col-1 my-auto">
                 </div>
             </div>
         </div>
