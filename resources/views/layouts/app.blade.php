28c1faf30705dbef2b925e20b273a16a0d089b98
Alex Christian <aquispe.developer@gmail.com>
2019-09-26 08:17:45 -0500

    Agregado bloque_1 de publicidad ADS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,11 @@
     </script>
 </head>
 <body>
+<!-- bloque_1 -->
+<ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-3673802025441541" data-ad-slot="5332221143" data-ad-format="auto" data-full-width-responsive="true"></ins>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 <div id="app">
 
     <!-- Social Share Kit -->
